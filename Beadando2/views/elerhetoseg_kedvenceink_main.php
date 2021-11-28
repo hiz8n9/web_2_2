7b2d96dca4376c9656bca546441806118c8be72e
hiz8n9 <94836438+hiz8n9@users.noreply.github.com>
2021-11-28 14:46:37 +0100

    Add files via upload
    Core

---
+++ Beadando2/views/elerhetoseg_kedvenceink_main.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+<meta charset="utf-8"/>
+</head>
+
+
+<h2 style="text-align: center">
+    <br><br><br>Kedvenceink
+</h2>
+
+<br><br>
+
+<h4 style="text-align: center">Rigójancsi</h4>
+<p style="text-align: justify; width: 80%; margin:auto">
+A rigójancsi egy hagyományos magyar, általában kocka alakú, csokoládéspiskótatészta- és csokoládékrém-alapú cukrászsütemény. A nevét Rigó Jancsi (1858–1927) híres magyar cigányprímásról kapta, aki elcsábította és feleségül vette Clara Ward, Caramane-Chimay hercegnőt, az amerikai milliomos E. B. Ward egyetlen lányát, aki Joseph Caraman-Chimay belga herceg felesége volt.
+</p>
+<br>
+
+<h4 style="text-align: center">Bledi krémes</h4>
+<p style="text-align: justify; width: 80%; margin:auto">A bledi krémes (szlovén neve blejska kremšnita) Bled városának és Szlovéniának is a szimbóluma, melyet Bled lakosai és az odalátogató turisták is rendszeresen fogyasztanak. 1953-ban a Park szálloda cukrászatában készítette először a magyar származású Ištvan Lukačevič (Lukacsevics István), és a szállodával szembeni kávézóban jelenleg is kapható.
+</p>
+<br>
+
+<h4 style="text-align: center">Sacher torta</h4>
+<p style="text-align: justify; width: 80%; margin:auto">A Sacher-torta az osztrák gasztronómia és Bécs városának egyik legismertebb süteménye. Egyfajta csokoládétorta, amely két réteg sűrű, nem túl édes csokoládés piskótatésztából és a közöttük lévő vékony réteg sárgabaracklekvárból áll. A tetejét és az oldalát csokoládébevonat borítja. A híres monarchiabeli cukrászkülönlegességek egyike. Korabeli nevezetes sütemények még a dobostorta és az Esterházy-torta.</p>
+<br>
+
